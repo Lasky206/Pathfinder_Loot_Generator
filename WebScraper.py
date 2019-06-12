4adec94cfab8e33304f8f63e744efb3c49e70139
Lasky206 <mistermisterio1000@gmail.com>
2019-06-12 01:22:58 -0600

    v0.0.4 -- working for potions --

--- WebScraper.py
+++ WebScraper.py
@@ -1,5 +1,7 @@
 import d20pfsrd_classes
 
-d21 = d20pfsrd_classes.d20pfsrd(6)
+d21 = d20pfsrd_classes.d20pfsrd(6,'https://www.d20pfsrd.com/magic-items/potions')
+method_list = d21.dict_convertion(d21.list_generator())
 
-print(d21.list_generator())
+for i in range(len(method_list)):
+    print(method_list[i])
--- d20pfsrd_classes.py
+++ d20pfsrd_classes.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 
 class d20pfsrd:
-    def __init__(self,tabnum):
-        self.response = requests.get('https://www.d20pfsrd.com/magic-items/potions').content
+    def __init__(self,tabnum,url):
+        self.response = requests.get(url).content
         self.unicode_str = self.response.decode('utf8')
         self.soup = BeautifulSoup(self.unicode_str, 'html.parser')
 
@@ -11,6 +11,7 @@ class d20pfsrd:
         self.table_tr = self.table.findAll('tr')
 
         self.list1 = []
+        self.list2 = []
 
     def list_generator(self):
         for i in self.table_tr:
@@ -18,8 +19,23 @@ class d20pfsrd:
             tmp = []
             for j in self.table_td:
                 table_clean = tmp.append(j.get_text())
-                self.list1.append(tmp)
+            self.list1.append(tmp)
+        self.list1.pop(0)
         return self.list1
 
-
-    # dict = {'minor':'', 'medium':'', 'major':'', 'Potion':'', 'Price':''}
+    def dict_convertion(self,input_list):
+        for item in input_list:
+            self.var1 = item[0]
+            self.var2 = item[1]
+            self.var3 = item[2]
+            self.var4 = item[3]
+            self.var5 = item[4]
+            # self.var4 = {
+            #     'Minor':self.var1,
+            #     'Medium':self.var2,
+            #     'Major':self.var3,
+            #     'Potion':self.var4,
+            #     'Price':self.var5
+            # }
+            self.list2.append({'Minor':self.var1, 'Medium':self.var2, 'Major':self.var3, 'Potion':self.var4, 'Price':self.var5})
+        return self.list2
