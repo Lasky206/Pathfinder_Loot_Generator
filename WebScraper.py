3b3d6af21290bd3a703bfd94dcfd4b6021944756
Lasky206 <mistermisterio1000@gmail.com>
2019-06-13 02:14:57 -0600

    v1.0.0 -- Working for all r20 website tables --

--- WebScraper.py
+++ WebScraper.py
@@ -1,7 +1,11 @@
 import d20pfsrd_classes
 
 d21 = d20pfsrd_classes.d20pfsrd(6,'https://www.d20pfsrd.com/magic-items/potions')
-method_list = d21.dict_convertion(d21.list_generator())
+d22 = d20pfsrd_classes.d20pfsrd(4,'https://www.d20pfsrd.com/magic-items/rings')
+d23 = d20pfsrd_classes.d20pfsrd(5,'https://www.d20pfsrd.com/magic-items/staves')
 
-for i in range(len(method_list)):
-    print(method_list[i])
+# print(d22.list_generator(4,0,4))
+list1 = d22.list_generator(4,0,4)[0]
+print(list1['Ring'])
+
+print(d23.list_generator(3,0,3))
--- d20pfsrd_classes.py
+++ d20pfsrd_classes.py
@@ -2,40 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 
 class d20pfsrd:
-    def __init__(self,tabnum,url):
+    def __init__(self,table_num,url):
         self.response = requests.get(url).content
         self.unicode_str = self.response.decode('utf8')
         self.soup = BeautifulSoup(self.unicode_str, 'html.parser')
-
-        self.table = self.soup.findAll('table')[tabnum]
+        self.table = self.soup.findAll('table')[table_num]
         self.table_tr = self.table.findAll('tr')
 
-        self.list1 = []
-        self.list2 = []
+        # self.list1 = []
+        # self.list2 = []
 
-    def list_generator(self):
+    def list_generator(self,num_of_columns,start_column = -1,end_column = -1):
+        item_stats_list = []
+        #---------------------------------#
+        # Creates a list of a lists
+        # containing items attributes
+        #---------------------------------#
         for i in self.table_tr:
-            self.table_td = i.findAll('td')
+            self.table_td = i.findAll(['td','th'])
             tmp = []
-            for j in self.table_td:
-                table_clean = tmp.append(j.get_text())
-            self.list1.append(tmp)
-        self.list1.pop(0)
-        return self.list1
-
-    def dict_convertion(self,input_list):
-        for item in input_list:
-            self.var1 = item[0]
-            self.var2 = item[1]
-            self.var3 = item[2]
-            self.var4 = item[3]
-            self.var5 = item[4]
-            # self.var4 = {
-            #     'Minor':self.var1,
-            #     'Medium':self.var2,
-            #     'Major':self.var3,
-            #     'Potion':self.var4,
-            #     'Price':self.var5
-            # }
-            self.list2.append({'Minor':self.var1, 'Medium':self.var2, 'Major':self.var3, 'Potion':self.var4, 'Price':self.var5})
-        return self.list2
+            for j in range(start_column,end_column):
+                tmp.append(self.table_td[j].get_text())
+            item_stats_list.append(tmp)
+        key = (item_stats_list[0])
+        item_stats_list.pop(0)
+        #----------------------------------#
+        # Creates a list of dictionaries
+        # containing items attributes
+        #----------------------------------#
+        item_stats_dict = []
+        for item in item_stats_list:
+            tmp = {}
+            for i in range(len(item)):
+                tmp[key[i]] = item[i]
+            item_stats_dict.append(tmp)
+        return item_stats_dict
