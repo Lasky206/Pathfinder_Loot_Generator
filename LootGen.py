0ee61853e960e5ec5feb0349330cc6c680bad834
Lasky206 <mistermisterio1000@gmail.com>
2019-06-21 01:41:31 -0600

    v0.1.3 -- Menu WIP --

--- LootGen.py
+++ LootGen.py
@@ -1,18 +1,22 @@
 import d20pfsrd_classes
-import random
+from menu import startup, main_menu
 
-# d21 = d20pfsrd_classes.d20pfsrd(6,'https://www.d20pfsrd.com/magic-items/potions')
-# d22 = d20pfsrd_classes.d20pfsrd(4,'https://www.d20pfsrd.com/magic-items/rings')
-# d23 = d20pfsrd_classes.d20pfsrd(5,'https://www.d20pfsrd.com/magic-items/staves')
-# wr1 = d20pfsrd_classes.scribe()
+#------------------ Sources -------------------#
+d21 = d20pfsrd_classes.d20pfsrd(6,'https://www.d20pfsrd.com/magic-items/potions')
+d22 = d20pfsrd_classes.d20pfsrd(4,'https://www.d20pfsrd.com/magic-items/rings')
+d23 = d20pfsrd_classes.d20pfsrd(5,'https://www.d20pfsrd.com/magic-items/staves')
+wr1 = d20pfsrd_classes.scribe()
 
+#-------- File handler for dynamic menu -------#
 fh1 = d20pfsrd_classes.File_Handler()
-menu_items = fh1.Open_File('menu.json')['Potions']
+menu_items = fh1.Open_File('menu.json')
 
-#----------- Create Example -------------------#
+#--------- Datacard Create Example ------------#
 # rings = d22.list_generator(4,0,3)
 # wr1.write_json(rings,'rings.json')
 #----------- Append Example -------------------#
 # wr1.append_json('rings.json',[{'fake':'list'},{'fake2':'list2'}])
 
-print(menu_items)
+
+startup()
+main_menu()
---
+++ menu.py
@@ -0,0 +1,31 @@
+import d20pfsrd_classes
+import os
+import sys
+import time
+
+#-------- File handler for dynamic menu -------#
+fh1 = d20pfsrd_classes.File_Handler()
+menu_items = fh1.Open_File('menu.json')
+
+#-------------- Startup Message ---------------#
+def startup():
+    os.system('clear')
+    print('''
+
+        # # # # # # # # # # #
+        #  Pick some loot!  #
+        # # # # # # # # # # #
+    ''')
+    time.sleep(3)
+    os.system('clear')
+
+#------------------- Main Menu ---------------#
+def main_menu():
+    os.system('clear')
+    menu_keys = list(menu_items.keys())
+
+    print('****************** MAIN MENU ******************\n')
+    for i in range(len(menu_items.keys())):
+        print(str(i+1) + ') ' + menu_keys[i])
+
+    choice = input('\nEnter your selection here: ')
